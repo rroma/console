1504d06a0b9dd90f2f62dabd30fa9934237cb3ef
Roman Romanov <r.romanov.mail@yandex.ru>
2015-05-04 00:03:06 +0300

    ace added, scripts saving

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,14 +1,36 @@
 'use strict';
 
-var consoleApp = angular.module('consoleApp', []);
+var consoleApp = angular.module('consoleApp', ['ui.ace']);
 consoleApp.controller('ScriptListCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('scripts/scripts.json').success(function(data) {
+        if (data === '') {
+            data = [];
+        }
         $scope.scripts = data;
         $scope.tabs = [];
         $scope.activeTab = null;
         $scope.recentTabs = [];
         $scope.newScriptWithTab();
     });
+    $scope.save = function() {
+        if ($scope.scripts.indexOf($scope.activeTab) == -1) {
+            for (var i = 0; i < $scope.scripts.length; i++) {
+                if ($scope.scripts[i].name === $scope.activeTab.script.name) {
+                    // TODO refactor
+                    alert('Script with name '+$scope.activeTab.script.name+'already exists!');
+                    return;
+                }
+            }
+            $scope.scripts.push($scope.activeTab.script);
+            $http.post('server/save.php', $scope.scripts)
+                .success(function(data){
+                    alert('zal');
+                })
+                .error(function(data){
+                    alert('peth');
+                });
+        }
+    }
     $scope.selectTab = function(tab) {
         $scope.activeTab = tab;
         $scope.pushToRecentTabs(tab);
@@ -32,6 +54,8 @@ consoleApp.controller('ScriptListCtrl', ['$scope', '$http', function($scope, $ht
         var recent = $scope.recentTabs.pop();
         if (recent) {
             $scope.selectTab(recent);
+        } else {
+            $scope.newScriptWithTab();
         }
     }
     $scope.createScript = function() {
@@ -42,21 +66,26 @@ consoleApp.controller('ScriptListCtrl', ['$scope', '$http', function($scope, $ht
     }
     $scope.newScriptWithTab = function() {
         var script = $scope.createScript();
-        $scope.scripts.push(script);
+        var tab = { "script": script };
+        $scope.tabs.push(tab);
+        $scope.selectTab(tab);
+    }
+    $scope.openScript = function(script) {
+        var tab = null;
+        $scope.tabs.forEach(function(item) {
+            if (item.script === script) {
+                tab = item;
+            }
+        });
+        if (tab) {
+            $scope.selectTab(tab);
+            return;
+        }
         var tab = { "script": script };
         $scope.tabs.push(tab);
         $scope.selectTab(tab);
     }
     $scope.getNewScriptName = function() {
-        var baseName = "New script ";
-        var num = "";
-        $scope.scripts.forEach(function(item){
-            var match = item.name.match(/^New\sscript(\s[0-9]+)?$/);
-            if (match) {
-                num = 1 in match && match[1] ? parseInt(match[1]) + 1 : 1;
-            }
-        });
-
-        return (baseName+num).trim();
+        return "New script";
     };
 }]);
